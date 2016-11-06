91d22d47ec92bc084418cde506c7e8024b14bb7b
Sibani Mohanty <mhnty.bani@gmail.com>
2016-11-07 00:34:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -34,13 +34,13 @@ app.get('/test-db',function(req,res){
 });
 
 app.get('/article/:articleName', function(req,res){
-    var articleName = req.params.articleName;
+    //var articleName = req.params.articleName;
     
  pool.query("SELECT * FROM article WHERE title ="+req.params.articleName,function(err,result){
      if(err){
          res.status(500).send(err.toString());
      }else{
-         if(result.rows.length == 0){
+         if(result.rows.length === 0){
              res.status(404).send('Article not found');
              
          }else{
