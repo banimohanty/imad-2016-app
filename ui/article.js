0e49f875f3dd08d89caeda2a9a0c00f75e80890f
Sibani Mohanty <mhnty.bani@gmail.com>
2016-11-09 23:41:05 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -1,7 +1,6 @@
 // Eg: coco98.imad.hasura-app.io/articles/article-one will result in article-one
 var currentArticleTitle = window.location.pathname.split('/')[2];
-loadLogin();
-loadComments();
+
 function loadCommentForm () {
     var commentFormHtml = `
         <h5>Submit a comment</h5>
@@ -97,4 +96,5 @@ function loadComments () {
 
 
 // The first thing to do is to check if the user is logged in!
-
+loadLogin();
+loadComments();
